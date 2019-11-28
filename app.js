b574b4f3fb0a2d0f5fb9297b59454eeebe896c13
Minh Mai <minhmanmai90@gmail.com>
2019-11-29 09:52:10 +1100

    Description and value fields validation

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ var UIController = (function () {
                 // Receive the type of the amount, either inc (income) or exp (expense)
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
 
@@ -125,24 +125,36 @@ var appController = (function (budgetCtrl, UICtrl) {
         })
     }
 
+    var updateBudget = function () {
+
+        // 1. Calculate budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+
+    };
+
     var ctrlAddItem = function () {
         var input, newItem;
 
         // 1. Get input data
         var input = UIController.getInput();
 
-        // 2. Add item to the budget controller
-        var newItem = budgetController.addItem(input.type, input.description, input.value);
+        if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+            // 2. Add item to the budget controller
+            var newItem = budgetController.addItem(input.type, input.description, input.value);
 
-        // 3. Add item to the UI
-        UIController.addListItem(newItem, input.type);
+            // 3. Add item to the UI
+            UIController.addListItem(newItem, input.type);
 
-        // 4. Clear UI input fields
-        UIController.clearFields();
+            // 4. Clear UI input fields
+            UIController.clearFields();
 
-        // 5. 
+            // 5. Update the budget
+            updateBudget();
 
-        // 6. Display the budget on the UI
+        }
     }
 
     return {
